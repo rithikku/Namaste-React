ae98ed0c5318dd613a62ec5ee0a5399f24a9e8a0
Rithik <rithikmac13@gmail.com>
2025-08-06 11:47:34 +0530

    4th Commit

--- README.md
+++ README.md
@@ -20,10 +20,4 @@
 - Tree Shaking - remove unused Code
 - Different dev and production bundles
 
-# React.createElement => ReactElement- JS Object => HTMLElement(render)
 
-const heading = React.createElement(
-    "h1", 
-    {id: "heading"}, 
-    "Welcome to React!"
-);
--- app.js
+++ app.js
@@ -1,39 +1,101 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom/client";
 
-// JSX - HTML - like or XML - like Syntax
+/**
+ * Header
+ *  - Logo
+ *  - Nav Items
+ * Body
+ *  - Search
+ *  - Restaruant Container
+ *      - Restaurant Card
+ *          - Cards 
+ *          - Images
+ *          - Res Name
+ *          - Star Rating
+ *          - Cuisine
+ *          - Price
+ *          - Delivery Time
+ * Footer
+ *  - Copyright
+ *  - Links
+ * - Contact
+ */
 
-// JSX (transpiled before it reaches the JS Engine) - PARCEL - Babel 
-
-// JSX => Babel transpiles it to React.createElement => ReactElement- JS Object => HTMLElement(render)
-
-// React Element
-const Heading = (<h1 id="heading">
-    Namaste React Using JSX
-    </h1>
-);
-
-// React Functional Component = Normal JS Function
-const TitleComponent = () => {
+// Header Component
+const Header = () => {
     return (
-        <div id="container">
-            <h1 id="heading">Title Component!</h1>
+        <div className="header">
+            <div className="logo-container">
+                <img className="logo" src="https://bcassetcdn.com/public/blog/wp-content/uploads/2019/07/18094833/the-red-cafe.png"/>
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li><img className="cart-logo" src="https://img.icons8.com/?size=160&id=QVQY51sDgy1I&format=png"/></li>
+                    <li><button className="login-button">Login</button></li>
+                </ul>
+            </div>
         </div>
     )
 }
 
-// Component Composition 
-const HeadingComponent = () => {
+// Restaurant Card Component 
+const RestaurantCard = () => {
     return (
-        <div id="container">
-            {Heading}
-            <TitleComponent/>
-            <h2 id="heading">{100 + 200}</h2>
-            <h1 id="heading">Namaste React Functional Component!</h1>
+        <div className="res-card">
+            <img className="res-card-img" src="https://images.pexels.com/photos/708488/pexels-photo-708488.jpeg?_gl=1*bjey0s*_ga*NTM5ODI2MTU3LjE3NTE2MTc3ODM.*_ga_8JE65Q40S6*czE3NTQ0NTg2MTMkbzMkZzEkdDE3NTQ0NTg2MjMkajUwJGwwJGgw"></img>
+            <h3>Meghna Foods</h3>
+            <h4>Biryani, North Indian, Asian</h4>
+            <h4>4.4 Stars</h4>
+            <h4>38 Minutes</h4>
+        </div>
+    )
+}
+
+// Body Component
+
+const Body = () => {
+    return (
+        <div className="body">
+            <div className="search">
+                <label></label>
+                <input type="search" className="search"></input>
+                <button>Search</button>
+            </div>
+            <div className="res-container">
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+            </div>
+        </div>
+    )
+}
+
+const AppLayout = () => {
+    return (
+        <div className="app">
+            <Header/>
+            <Body/>
         </div>
     )
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<HeadingComponent/>);
+root.render(<AppLayout/>);
