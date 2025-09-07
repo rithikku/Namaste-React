41b483b0f8ada51e60f3d9453c0054d5e369ca61
Rithik <rithikmac13@gmail.com>
2025-09-07 13:05:15 +0530

    22th Commit

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,13 @@
 const Contact = () => {
     return (
-        <h2>Contact Us Page</h2>
+        <div className="contact-div">
+            <h2>Contact Us Page</h2>
+            <form>
+                <input type="text" className="contact-name" placeholder="Name"></input>
+                <input type="text" className="contact-message" placeholder="Message"></input>
+                <button className="contact-button">Submit</button>
+            </form>
+        </div>
     )
 }
 
